df27e2e8751a8e3f4bce0d552fb6f4b72cafb58b
Michelle Caterine García Velasco <mitchcatg@gmail.com>
2025-11-23 17:35:33 -0600

    style: integra dependencias de sass, bulma, fontawesome e importa estilos base

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
+import './assets/sass/main.scss'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 const app = createApp(App)
 
+app.component('FontAwesomeIcon', FontAwesomeIcon);
 app.use(createPinia())
 app.use(router)
 
