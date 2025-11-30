bc51ad3984d0d9a81637b01109052ac492413c41
Michelle Caterine García Velasco <mitchcatg@gmail.com>
2025-11-29 18:56:28 -0600

    add: integra consulta básica de autor, strapi

--- src/views/RepositorioItemView.vue
+++ src/views/RepositorioItemView.vue
@@ -5,14 +5,36 @@
     <div v-if="isLoading">Cargando...</div>
     <div v-else-if="isError">Error al cargar los items.</div>
     <template v-else>
+      <hr>
+      items:
       <pre>
         {{ items }}
+      </pre>
+      <hr>
+      authors for item source /items/3:
+      <pre>
+        {{ authorsArray }}
       </pre>
     </template>
   </main>
 </template>
 <script setup>
+  import { ref } from 'vue'
+  // OmekaS items
   import { useSimpleItems } from '@/composables/useSimpleItems.js'
   const { isLoading, isError, items, fetchItems } = useSimpleItems()
   fetchItems()
+
+  // Strapi authors with items sources (linked OmekaS)
+  import { getAuthorsForItemSource } from '@/api/strapiService.js'
+  const authorsArray = ref([])
+  async function fetchAuthors() {
+    try {
+      const response = await getAuthorsForItemSource('/items/3')
+      authorsArray.value = response.data?.data?.[0]?.autores
+    } catch (error) {
+      console.error('Error fetching authors:', error)
+    }
+  }
+  fetchAuthors()
 </script>
