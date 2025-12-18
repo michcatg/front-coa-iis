25a37b42bded433697cca8265cb6500d81e27c70
Michelle Caterine García Velasco <mitchcatg@gmail.com>
2025-12-18 11:15:33 -0600

    chore: corrige ruta para acceder a la vista de items, items explore

--- src/router/index.js
+++ src/router/index.js
@@ -4,9 +4,9 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/repositorio/items/:id',
+      path: '/items/',
       name: 'item',
-      component: () => import('../views/RepositorioItemView.vue'),
+      component: () => import('../views/ExploreItems.vue'),
       props: true,
     }
   ],
