f0eadd1a2f3bb1a8cc658ced3e4dbad895ec95c9
Michelle Caterine García Velasco <mitchcatg@gmail.com>
2025-12-20 18:07:40 -0600

    add: agrega soporte a las variaciónes del campo de descripción para items que no son libros

--- src/application/adapters/itemAdapter.js
+++ src/application/adapters/itemAdapter.js
@@ -6,7 +6,9 @@ export function toItemResumeDto(apiData) {
     thumbnailSource: apiData["thumbnail_display_urls"] ? apiData["thumbnail_display_urls"]["medium"] : undefined,
     title: apiData['dcterms:title'][0]['@value'],
     createdAt: apiData['dcterms:created'] ? new Date(apiData['dcterms:created'][0]['@value']).toLocaleDateString('es-ES', { day: '2-digit', month: 'long', year: 'numeric' }) : '',
-    description: apiData['dcterms:description'] ? apiData['dcterms:description'][0]['@value'] : '',
+    description: apiData['dcterms:description'] ?
+      apiData['dcterms:description'][0]['@value'] : apiData['dcterms:abstract'] ?
+        apiData['dcterms:abstract'][0]['@value'] : '',
     authors: apiData['dcterms:creator'] ? apiData['dcterms:creator'].map(creator => creator['@value']) : [],
   });
 }
