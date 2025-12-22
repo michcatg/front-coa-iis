fc3293e9f077f81e1865a2cc2697f7b3b564e468
Michelle Caterine García Velasco <mitchcatg@gmail.com>
2025-12-22 14:03:17 -0600

    fix: agrega archivo partial view de facets perdido, explore items

---
+++ src/views/ExploreItems/Facets.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- Aside de facets -->
+  <aside class="column is-2" aria-label="Filtros">
+    <CategoriesFacet @update:model-value="emit('update:model-value', $event)" v-model="categoriesSelected" />
+  </aside>
+</template>
+<script setup>
+  import CategoriesFacet from './CategoriesFacet.vue'
+  const model = defineModel({
+    type: Array,
+    default: () => []
+  })
+  const emit = defineEmits(['update:model-value'])
+</script>
