f5062887cf2cc6b5208af702145b4d3b80beba28
Michelle Caterine García Velasco <mitchcatg@gmail.com>
2026-01-09 14:05:48 -0600

    add: agrega selector de categorías, item form

--- src/components/ItemForm.vue
+++ src/components/ItemForm.vue
@@ -1,5 +1,23 @@
 <template>
     <form @submit.prevent="handleFormSubmit">
+        <div class="field">
+            <label
+                class="label"
+                for="categoria"
+            >
+                Categoría
+            </label>
+            <div class="control">
+                <custom-select-input
+                    v-model="data.categoria"
+                    :options="categories"
+                    label-for="categoria"
+                    key-value="id"
+                    key-text="name"
+                    placeholder="Seleccione una categoría"
+                />
+            </div>
+        </div>
         <div>
             <label for="titulo">Título:</label>
             <input id="titulo" name="titulo" type="text" v-model="data.datosCatalogacion.title" />
@@ -22,6 +40,22 @@
 <script setup>
     import { reactive, ref } from 'vue'
     import { useCreateItem } from '@/composables/useCreateItem'
+    import {
+        SelectSercheable as CustomSelectInput,
+    } from 'vue-ui-kit'
+
+    const props = defineProps({
+        /**
+         * Lista de categorías disponibles para seleccionar
+         * @type {Array<{id: number, name: string}>}
+         * @example [{ id: 1, name: 'Libro' }, { id: 2, name: 'Artículo' }]
+         */
+        categories: {
+            type: Array,
+            required: true
+        }
+    })
+
     const createItem = useCreateItem()
 
     const data = reactive({
@@ -29,6 +63,7 @@
             title: ''
         },
         autores: [],
+        categoria: null,
     })
     const archivos = ref([])
     const handleFormSubmit = async () => {
