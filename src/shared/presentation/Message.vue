81d40b4b712ae1c9528e4906a72df2b7161f0d17
Michelle Caterine García Velasco <mitchcatg@gmail.com>
2026-01-14 08:34:00 -0600

    add: crea componente para unificar mensajes de estado

---
+++ src/shared/presentation/Message.vue
@@ -0,0 +1,20 @@
+<template>
+    <article class="message" :class="`is-${props.type}`">
+        <div class="message-body">
+            <slot></slot>
+        </div>
+    </article>
+</template>
+<script setup>
+import { defineProps } from 'vue'
+
+  const props = defineProps({    
+    type: {
+      type: String,
+      default: 'info' // link, primary, info, success, warning, danger
+    }
+  })
+</script>
+<style lang="scss" scoped>
+  @forward "bulma/sass/components/message";
+</style>
